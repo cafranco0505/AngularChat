71301485bb33a379c46101942987ec1c6b7a57cc
Carlos Franco <cafranco0505@gmail.com>
2020-11-23 21:52:17 -0500

    pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HealthComponent } from './assistants/health/health.component';
 import { ItSupportComponent } from './assistants/it-support/it-support.component';
 import { ShoppingComponent } from './assistants/shopping/shopping.component';
+import { NavComponent } from './pages/nav/nav.component';
+import { BodyComponent } from './pages/body/body.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HealthComponent,
     ItSupportComponent,
-    ShoppingComponent
+    ShoppingComponent,
+    NavComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule,
